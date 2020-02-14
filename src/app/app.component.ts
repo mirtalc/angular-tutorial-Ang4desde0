f571469388d1df410da0ea4df5e9488a5f97ac0f
Mirta <mirta.lopez.cardona@gmail.com>
2020-02-14 20:17:15 +0100

    feat(#3): Editar vista del app.component i afegir alguns comentaris

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
+/**
+ * 1. Imports
+ */
 import { Component } from '@angular/core';
 
+/**
+ * 2. Decorador del Component amb metadata
+ */
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    // etiqueta HTML on es renderitza el component
+    selector: 'app-root',
+    // arxiu HTML amb la vista
+    templateUrl: './app.component.html',
+    // arxiu(s) amb l'estil del component
+    styleUrls: ['./app.component.scss']
 })
+
+/**
+ * 3. Export
+ */
 export class AppComponent {
-  title = 'appCurso';
+    titol: string = 'appCurso';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
+/**
+ * 1. Imports
+ */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/**
+ * 2. Decorador del Module amb metadata
+ */
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    // declarations a.k.a. vistes d'un mòdul. Poden ser directives, components o pipes
+    declarations: [
+        AppComponent
+    ],
+    // dependències (l'origen està a la primera secció amb el imports)
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    // providers a.k.a. servicis disponibles per a tot component del Module
+    providers: [],
+    // Vista arrel. NOmés ho gasta el root module. No confundir amb la llibreria!
+    bootstrap: [AppComponent]
 })
+
+/**
+ * 3. Export
+ */
 export class AppModule { }
